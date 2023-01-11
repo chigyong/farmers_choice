dd2b1e6e173a75e038fb7f7f3a895dae1015d6d2
Rey  Baterbonia <poochinng@gmail.com>
2023-01-11 13:37:00 +0800

    initt

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const BTN = document.getElementsByClassName('menu')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const navbarLogin = document.getElementsByClassName('navbar-login')[0]
 
+
 BTN.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
     navbarLogin.classList.toggle('active')
     BTN.classList.toggle('active')
 })
+
