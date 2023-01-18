4498f49bef802c44530e1c6f6cdc495de54b53d5
Rey  Baterbonia <poochinng@gmail.com>
2023-01-18 13:26:13 +0800

    init

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ BTN.addEventListener('click', () => {
     navbarLogin.classList.toggle('active')
     BTN.classList.toggle('active')
 })
+
+
+//Shop Script
+
+
+
+function calculateBeef(){
+    let beef = parseFloat(document.getElementById('beef').value);
+    let sumofTotalBeef = beef * 600;
+    document.getElementById('demo').innerHTML = sumofTotalBeef;
+    document.getElementById('beef-demo').innerHTML = beef + ' Kilos of Beef';
+    
+}
+function calculateChicken(){
+    let chicken = parseFloat(document.getElementById('chicken').value);
+    let sumofTotalChicken = chicken * 200;
+    document.getElementById('demo').innerHTML = sumofTotalChicken;
+    document.getElementById('chicken-demo').innerHTML = chicken + ' Kilos of Chicken';
+     
+}
+function calculatePork(){
+    let pork = parseFloat(document.getElementById('pork').value);
+    let sumofTotalPork = pork * 300;
+    document.getElementById('demo').innerHTML = sumofTotalPork;
+    document.getElementById('pork-demo').innerHTML = pork + ' Kilos of Pork';
+
+}
+function calculateGoat(){
+    let goat = parseFloat(document.getElementById('goat').value);
+    let sumofTotalGoat = goat * 420;
+    document.getElementById('demo').innerHTML = sumofTotalGoat;
+    document.getElementById('goat-demo').innerHTML = goat + ' Kilos of Goat';
+
+}
